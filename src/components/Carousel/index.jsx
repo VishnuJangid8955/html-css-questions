f6b74b54aa81abc4dfbeef8e24a6d61ee3fea3eb
vishnu jangir <vishnu.jangir@qdegrees.com>
2023-07-10 07:16:40 +0530

    carousel left and right button

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -17,6 +17,45 @@ const Carousel = () => {
           <p>Senior Software Engineer</p>
         </div>
       </div>
+      <button className="btn btn-left">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="icon"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M15.75 19.5L8.25 12l7.5-7.5"
+          />
+        </svg>
+      </button>
+      <button className="btn btn-right">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="icon"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M8.25 4.5l7.5 7.5-7.5 7.5"
+          />
+        </svg>
+      </button>
+
+      <div className="dots">
+        <button className="dot">&nbsp;</button>
+        <button className="dot">&nbsp;</button>
+        <button className="dot">&nbsp;</button>
+        <button className="dot">&nbsp;</button>
+      </div>
     </div>
   );
 };
